7ef5b404dc0f6e6b2980a25bd1a265edda9e0679
kwonjounghun <kjh0102321@gmail.com>
2024-12-27 22:20:14 +0900

    refactor: next.config.js를 TypeScript로 마이그레이션하고 로그인 페이지 추가
    - 기존의 next.config.js 파일을 제거하고 next.config.ts로 대체하여 타입 안정성을 강화했습니다.
    -	Google 로그인 기능을 포함한 새로운 로그인 페이지 컴포넌트를 추가했으며, 로그인 후 사용자 리디렉션 기능을 구현했습니다.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'api.ts'],
+  webpack: (config: any, { dev, isServer }: { dev: boolean, isServer: boolean }) => {
+    // 테스트 파일과 스토리북 파일 제외
+    config.module.rules.push({
+      test: /\.(test|spec|stories)\.(ts|tsx)$/,
+      loader: 'ignore-loader',
+    });
+
+    return config;
+  },
 };
 
-export default nextConfig; 
+module.exports = nextConfig;
